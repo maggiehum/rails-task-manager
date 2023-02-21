07ddbe56f6165d00a807581c04566409d75bb81d
Maggie hum <hum.maggie@gmail.com>
2023-02-21 14:36:25 -0500

    task manager done

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,6 @@ Rails.application.routes.draw do
   get 'tasks/:id', to: 'tasks#show', as: :task
   patch 'tasks/:id', to: 'tasks#update'
 
-
+  delete 'tasks/:id', to: 'tasks#destroy'
   root to: 'tasks#index'
 end
